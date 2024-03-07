7b9a9aa72d40462aba1ca4c9fb75b9d8f5403723
Pushkar <pushkarcdn@gmail.com>
2024-03-07 14:36:35 +0545

    made mobile nav

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 const searchExpandedDiv = document.querySelector('.search-expanded-div');
 const navSearchButton = document.querySelector('.nav-search-button-div');
-
+const expandMenuButton = document.querySelector('.header-3dot-div');
+const mobileNav = document.querySelector('.mobile-nav');
 
 navSearchButton.addEventListener('click', () => {
     navSearchButton.style.visibility = 'hidden';
     searchExpandedDiv.style.visibility = 'visible';
+});
+
+mobileNav.style.left = "-350px";
+
+expandMenuButton.addEventListener('click', () => {
+
+    if (mobileNav.style.left == "-350px") {
+        mobileNav.style.left = '-1px';
+    } else {
+        mobileNav.style.left = '-350px';
+    }
 });
