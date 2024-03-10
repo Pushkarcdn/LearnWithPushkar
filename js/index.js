dafac44d7918c24027d8149550c96a8417f972ba
Pushkar <pushkarcdn@gmail.com>
2024-03-10 12:37:56 +0545

    second section done

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
+// -------------------------------------- searchbox on desktop -------------------------------------
+
 const searchExpandedDiv = document.querySelector('.search-expanded-div');
 const navSearchButton = document.querySelector('.nav-search-button-div');
-const expandMenuButton = document.querySelector('.header-3dot-div');
-const mobileNav = document.querySelector('.mobile-nav');
 
 navSearchButton.addEventListener('click', () => {
     navSearchButton.style.visibility = 'hidden';
     searchExpandedDiv.style.visibility = 'visible';
 });
 
-// mobile nav
+
+// -------------------------------------- navbar on mobile -------------------------------------
+
+const expandMenuButton = document.querySelector('.header-3dot-div');
+const mobileNav = document.querySelector('.mobile-nav');
 
 function openMobileNav() {
     mobileNav.style.right = '0px';
